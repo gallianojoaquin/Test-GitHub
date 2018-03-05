2e7fe72c4e50f019e726da5c0a49eb20c98f9cfa
gallianojoaquin <gallianojoaquin@gmail.com>
2018-03-05 11:50:48 -0300

    Agrego atributos a clase Category

--- grails-app/domain/test/github/Category.groovy
+++ grails-app/domain/test/github/Category.groovy
@@ -3,9 +3,17 @@ package test.github
 class Category {
 
     String name
+    String urlImage
+    Integer visits
 
     static belongsTo = [site:Site]
 
     static constraints = {
+        name nullable: false, blank: false, unique: true
+        visits validator: { Integer val ->
+            if(val < 0) return 'visitsCanNotBeLowerThanZero'
+        }
+        urlImage  blank: true
+        site nullable: true
     }
 }
