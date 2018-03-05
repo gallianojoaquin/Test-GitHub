0620ff2198944d86a69cba4531e4b6a23d0b7ae4
Eric <eric.dominguez@live.com.ar>
2018-03-05 11:40:49 -0300

    Agregado parametros del user y sus constraints necesarios

--- grails-app/domain/test/github/User.groovy
+++ grails-app/domain/test/github/User.groovy
@@ -1,9 +1,15 @@
 package test.github
 
 class User {
+    String user
+    String password
     String name
     String lastName
 
     static constraints = {
+        user size: 5..15, blank: false, unique: true
+        password size: 5..15, blank: false
+        name blank: false
+        lastName blank: false
     }
 }
